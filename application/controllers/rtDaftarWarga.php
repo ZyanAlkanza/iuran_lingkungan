1d0deece8f1219556c87f42231215b8b33974177
ZyanAlkanza <zyan.mujaddid@gmail.com>
2023-06-29 14:05:36 +0700

    membuat halaman rtDaftarWarga_tambah

--- application/controllers/rtDaftarWarga.php
+++ application/controllers/rtDaftarWarga.php
@@ -5,6 +5,11 @@
             $data['warga'] = $this->db->query("SELECT * FROM warga WHERE warga.role='3'")->result();
             $this->load->view('rtDaftarWarga', $data);
         }
+
+        public function tambah()
+        {
+            $this->load->view('rtDaftarWarga_tambah');
+        }
     }
 
 ?>
